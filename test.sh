d34b51634ad1bd93f7f4198efacb7d71be193191
sunilkr <badboy16a@gmail.com>
2018-11-05 12:18:33 +0530

    correnting for sh

--- test.sh
+++ test.sh
@@ -6,14 +6,14 @@ echo $1
 if readelf -s "$1" 2>/dev/null | grep -q '__stack_chk_fail'; then
     echo "Has stack check"
 else
-    ((RES++))
+    RES=$((RES + 1))
     echo 'No stack check'
 fi
 #echo $RES
 
 if readelf --symbols "$1" 2>/dev/null | grep -q '.symtab'; then
     echo "Has symtab"
-    ((RES++))
+    RES=$((RES + 1))
 else
     echo "No symtab"
 fi
